e3e8f5a3f67ad2e172869d5bc01d78efeceee751
Juan Camilo Flórez <jcflorezv@unal.edu.co>
2020-09-10 21:00:40 -0500

    integrals with dot product

--- lab1.m
+++ lab1.m
@@ -3,44 +3,21 @@ clear
 
 
 f_saw = @(t) 2 * mod(t, 5);
-% fplot(f, [-10 10])
+fplot(f, [-10 10])
 
-% t = -2*pi : 0.001 : 2*pi;
-% plot(t, square(t))
-
-% t = linspace(0,3*pi)';
-% x = square(t);
-
-% Script OCTAVE para la serie de Fourier de una onda periódica cuadrada.
-
-t=[-1:0.01:1];
-f_wave = inline('(t >= 0) & (t < 1)', 't')
+N = 10
+t=[-1:0.01:2];
+f_wave = inline('(t >= 0)', 't')
 T_0 = 1;
 f_wave_06 = f_wave(-1) %% should equal 0
-% plot(t, f_square(t)); axis([-2 2 -2 2]);
+%% plot(t, f_wave(t)); axis([-2 2 -2 2]);
 
-function integrand(t_i)
-  f_wave(5)*cos(n*w_0*5)
-endfunction
-
-n = 1
-t_0 = 0;
-w_0 = 2*pi/T_0;
-a = quad(integrand, t_0, t_0 + T_0)
-
-
-
-function square(t)
-  t=t*(1/(pi));
-  y=ones(size(t));
-  y(find(bitand(abs(floor(t)),1)))=-1;
+for i=1:N
   
-  %t = -0.02:.0001:.0625;
-  %y = square(t);
-  %plot(t,y,'-o')
-end
+endfor
 
-fourierTrigonometricSeries(4, f_square, 0, 1)
+
+
 
 
 
--- ss.m
+++ ss.m
@@ -1,23 +1,24 @@
 clc
 clear 
+
+%% PROGRAM PARAMS
 L = pi;
 N = 1024;
 dx = 2*L/(N-1);
 x = -L:dx:L;
+T0 = 2*L;
 
+%% WAVE FUNCTION
 f = 0*x;
 % f(N/4:N/2) = 4*(1:N/4+1)/N;
 % f(N/2+1:3*N/4) = 1-4*(0:N/4-1)/N;
 f(N/4:3*N/4) = 1;
 % plot(x,f,'-k','LineWidth',1.5); hold on; axis([-2 2 -2 2])
 
+%% 
 CC = jet(20);
-A0 = quad('f', -L, L)
-
-
-
-
-
-
-
+A0 = sum(f.*ones(size(x)))*dx;
+for k=1:20
+  A(k) = sum(f.*cos(2*pi*k*x/T0))*dx;
+endfor
 

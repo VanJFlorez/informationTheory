4b0e2011de343d0e95b3da4fc4e53557c972b612
Juan Camilo Florez <jcflorezv@unal.edu.co>
2020-12-12 12:10:24 -0500

    added samples to play

--- FinalProject/baseProject.m
+++ FinalProject/baseProject.m
@@ -1,5 +1,10 @@
 clc
 clear
+#######
+## Instalar el paquete de entropia con
+## pkg install -forge informationtheory
+## pkg load informationtheory
+#######
 
 % H < K < H + c
 % 000
@@ -8,8 +13,21 @@ clear
 % 011
 % 100
 % 101
-K = kolmogorov('ABCDEE')
+K = kolmogorov('ABCDE')
 H = entropy([1/6 1/6 1/6 1/6 1/6])
+
+H < K
+H - K
+
+
+K = kolmogorov('XYZ')
+H = entropy([1/3 1/3 1/3])
+
+H < K
+H - K
+
+K = kolmogorov('00011011')
+H = entropy([1/4 1/4 1/4 1/4])
 
 H < K
 H - K

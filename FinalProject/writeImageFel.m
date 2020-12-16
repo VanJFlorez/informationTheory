d0ad92a27c33679aa71250f3b7fca51253d4f406
AndresFelipeCastellanos <45008613+AndresFelipeCastellanos@users.noreply.github.com>
2020-12-16 11:29:54 -0500

    Create writeImageFel.m

---
+++ FinalProject/writeImageFel.m
@@ -0,0 +1,35 @@
+function wImage=writeImageFel(txt)
+
+  %txt - Cadena del archivo de texto
+  
+  n = length(txt)
+  pixelsN = ceil(sqrt(n));   #Para la matriz imagen, la cual sera cudrada
+  left = mod(n,pixelsN)
+  
+  for i=1:left
+    txt=strcat(txt,"0")
+  endfor
+  
+  txt
+  
+  r = zeros(pixelsN, pixelsN)
+  g = zeros(pixelsN, pixelsN)
+  b = zeros(pixelsN, pixelsN)
+  
+  for i=1:pixelsN
+    
+    for j=1:pixelsN
+    
+      r(i,j)= (toascii(txt(i+j)) - 97)/27;
+      
+    endfor
+    
+  endfor  
+  
+  r
+  
+  fig=gray2ind (r);
+  image(fig);
+  imwrite(fig, "img.jpg") ;
+  
+endfunction

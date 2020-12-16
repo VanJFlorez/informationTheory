2604be578ced4de72d3dc093908ee3d539f72279
AndresFelipeCastellanos <45008613+AndresFelipeCastellanos@users.noreply.github.com>
2020-12-16 14:17:52 -0500

    Files update

--- FinalProject/fase2.m
+++ FinalProject/fase2.m
@@ -1,6 +1,25 @@
-clc
-clear
-pkg load statistics
+##########################################################################
+# Fase 2
+# Genero cadenas con diferentes generadores aleatorios. Voy a ver la aleatoriedad
+# de cada uno a partir de los valores obtenidos con la funcion kolmogorov.
+# En este caso usamos Kolmogorov para medir aleatoriedad (randomness).
+#
+# Espero que entre más kolmogorov -> más sofisticado es el generador -> más complejidad
+# -> más ruido tendrá mi imagen. 
+# Ejemplo del ranking
+# -Purely random DEFAULT octave
+# -Twisted (pseudorandom)
+# -Beta Distribution	betarnd
+# -Binomial Distribution	binornd
+# -Cauchy Distribution	cauchy_rnd
+# -Chi-Square Distribution	chi2rnd
+# -Univariate Discrete Distribution	discrete_rnd
+# -Empirical Distribution	empirical_rnd
+# -Exponential Distribution	exprnd
+# -F Distribution	frnd
+# -Gamma Distribution	gamrnd
+##########################################################################
+
 N = 35*35; #   5   5   7   7
 strings = [];
 #       rep -----------> purely random
